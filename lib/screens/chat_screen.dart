86be1f6b0de8f8d1da16f34f738e3eaa461cb737
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-08-06 20:26:02 +0100

    Handling Push Notifications - FIREBASE Cloud Messaging

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
 
-
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import '../widgets/chats/new_message.dart';
 import '../widgets/chats/messages.dart';
 
-
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({Key key}) : super(key: key);
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    final fbm = FirebaseMessaging(); //used below to handle notifications
+    fbm.requestNotificationPermissions(); //requiered only on ios
+    fbm.configure(onMessage: (msg) {//wehn app is opened in use mode
+      //configure - do something depending where you are when the notificaiton is received (app closed running in bacground, opened)
+      print(msg);
+      return;
+    }, 
+    onResume: (msg) {//when app is opened in bacground
+      print(msg);
+      return;
+    }, 
+    onLaunch: (msg) {
+      print(msg);
+      return;
+    });
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,7 +77,6 @@ class ChatScreen extends StatelessWidget {
           NewMessage(),
         ],
       )),
-      
     );
   }
 }
